c131f77b62d521b65da5e49245aa4bddeb9d47e5
89610 <89610@DESKTOP-VTT1R58>
2023-02-15 00:53:58 +0300

    Добавьте файлы проекта.

---
+++ 2.30_/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        int x = 456;
+        int n = x - (x % 100) + ((x % 10) * 10) + (((x % 100) - (x % 10)) / 10);
+        Console.WriteLine(n);
+    }
+}
